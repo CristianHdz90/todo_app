d301037628c7d023d9e3f5eea6944b3d75a45797
Camilo-Linux <hernandezcamilo063@gmail.com>
2021-08-05 16:40:26 -0500

    Functions added in firestore_service file

--- src/app/firestore_service.py
+++ src/app/firestore_service.py
@@ -1,16 +1,43 @@
 from google.cloud import firestore
 
+
 db = firestore.Client(project='todo-app-321822')
 
-#USERS
+
+#USERS (C,R,U,D)
+
+def add_user(new_user, data):
+    db.collection('users').document(new_user).set(data)
 
 
-#In order to get users
 def get_users():
     return db.collection('users').get()
 
 
-#TO DOS
+def get_user(user_id):
+    return db.collection('users').document(user_id).get()
 
-def get_todos():
-    pass
+
+def delete_user(user_id):
+    db.document(f'users/{user_id}').delete()
+
+     
+
+
+#TO DO (C,R,U,D)
+
+def add_todo(user_id, todo):
+    todo_ref = db.collection('users').document(user_id).collection('todos')
+    todo_ref.add(todo)
+
+
+def get_todos(user_id):
+    return db.collection('users').document(user_id).collection('todos').get()
+
+
+def update_todo(user_id, todo_id, new_data):
+    db.collection('users').document(user_id).collection('todos').document(todo_id).update(new_data)
+
+
+def delete_todo(user_id, todo_id):
+    db.collection('users').document(user_id).collection('todos').document(todo_id).delete()
--- src/main.py
+++ src/main.py
@@ -1,18 +1,39 @@
 from flask import render_template, request
 from app import create_app
-from app.firestore_service import get_users
+#CRUD of users
+from app.firestore_service import add_user, get_user, delete_user
+#CRUD of to-do's
+from app.firestore_service import add_todo, get_todos, update_todo, delete_todo
+
 
 app = create_app()
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home", methods=['GET'])
 def home():
 
     user_ip = request.remote_addr
     
-    #To get users
-    users = get_users()
-    print(users)
+    #Adding to-do's
+    
+    # todo = {
+    #     'task 2':'xxxxxx'
+    # }
+    # add_todo('camilo', todo)
+
+    #Get todos
+    # get =  get_todos('camilo')
+    # for i in get:
+    #     print(i.to_dict())
+
+    #Update todo #
+    # update_todo('camilo', 'lRyK6Ze0Buz4hCJcGyF2', {'descriptio': 'xxxxxxx'})
+
+    #Delete todo
+    # delete_todo('camilo', 'JjBDZf8BZB0yEOlfJIVG')
+
+
+
 
     context = {
         "saludo": "Hello word",
